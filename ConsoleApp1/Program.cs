e9234958b490703c4ea3e0c8d1b0532f5eae4de7
tahatk22 <tk2595922@gmail.com>
2023-11-18 21:54:28 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter A Sambol");
+        string Sambol = Console.ReadLine();
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(Sambol);
+            }
+            Console.WriteLine();
+        }
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(Sambol);
+            }
+            Console.WriteLine();
+        }
+    }
+}
